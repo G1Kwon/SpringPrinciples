4999f53db3c6050f619a53690d9100269bbbd41d
g1.kwon <spring_shot@naver.com>
2021-03-22 00:14:16 +0900

    bean life cycle callback

---
+++ src/test/java/core/principles/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,31 @@
+package core.principles.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        //관계 : ApplicationContext -> ConfigurableApplicationContext -> AnnotationConfigApplicationContext
+        ConfigurableApplicationContext applicationContext = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = applicationContext.getBean(NetworkClient.class);
+        applicationContext.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+//        @Bean(initMethod = "afterPropertiesSet", destroyMethod = "destroy")
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
